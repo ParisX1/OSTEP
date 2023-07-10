559b516f6e4e2eda65239574d79522ff8b379c46
Tom O'Mara <tom@winerepublic.com.au>
2023-07-11 01:03:51 +1000

    wzip passing all tests

---
+++ Projects/Initial_Utilities/wzip/nulltest.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+	//printf("%c", '\0');
+	return 0;
+}
--- Projects/Initial_Utilities/wzip/wzip.c
+++ Projects/Initial_Utilities/wzip/wzip.c
@@ -15,7 +15,8 @@ https://github.com/remzi-arpacidusseau/ostep-projects/tree/master/initial-utilit
 #include <string.h>
 
 #define CHARS_TO_WRITE 1
-#define BUFFER_SIZE 1
+#define BUFFER_SIZE 2
+#define CHAR_SIZE 1
 
 int main(int argc, char* argv[]) {
     if (argc <= 1) {
@@ -29,41 +30,39 @@ int main(int argc, char* argv[]) {
     char currentChar[BUFFER_SIZE];
     int charCount = 1;
 
+    /*
+    If the count is "1" then that still needs to be included
+    That also counts for the return char
+    */
+
     for (int i = 1; i < argc; i++) {
         fileStreamIn = fopen(argv[i], "r");
 
         if (fileStreamIn != NULL) {
-
-            //if (i == 1) prevChar = (char) fgetc(fileStreamIn); // Get first char in the first file
-            //charCount = 1;
-            if (i == 1) fread(prevChar, BUFFER_SIZE, sizeof(char), fileStreamIn); // Get first char in the first file
-            //printf("prev: %s \n", prevChar);
-            //printf("current: %s \n", currentChar);
-            while (fread(currentChar, BUFFER_SIZE, sizeof(char), fileStreamIn)) {
+            if (i == 1) fread(prevChar, CHAR_SIZE, sizeof(char), fileStreamIn); // Get first char in the first file
+            while (fread(currentChar, CHAR_SIZE, sizeof(char), fileStreamIn)) {
+                
                 //printf("prev: %s \n", prevChar);
                 //printf("current: %s \n", currentChar);
+                
                 if (currentChar[0] != prevChar [0]) {
-                    //printf("prev: %s \n", prevChar);
-                    //printf("current: %s\n", currentChar);
-                    //printf("chars are diff\n");
-                    if (charCount > 1) {
+                    if (prevChar[0] != '\0') {
+                        //fwrite(&charCount, sizeof(int), CHARS_TO_WRITE, fileStreamOut);
                         fwrite(&charCount, sizeof(int), CHARS_TO_WRITE, fileStreamOut);
-                        charCount = 1;
+                        fwrite(prevChar, sizeof(char), CHARS_TO_WRITE, fileStreamOut);   
                     }   
-                    //fwrite(prevChar, sizeof(char), CHARS_TO_WRITE, fileStreamOut);
-                    //prevChar[0] = currentChar[0];
-                    strcpy(prevChar, currentChar);
+                    
+                    charCount = 1;
+                    prevChar[0] = currentChar[0];
                 }
                 else {
                     charCount++;
                 }
             }
-            //if (charCount > 1) fwrite(&charCount, sizeof(int), CHARS_TO_WRITE, fileStreamOut);
-            //fwrite(&prevChar, sizeof(char), CHARS_TO_WRITE, fileStreamOut);
             fclose(fileStreamIn);
         }
     }
-    if (charCount > 1) fwrite(&charCount, sizeof(int), CHARS_TO_WRITE, fileStreamOut);
+    fwrite(&charCount, sizeof(int), CHARS_TO_WRITE, fileStreamOut);
     fwrite(prevChar, sizeof(char), CHARS_TO_WRITE, fileStreamOut);
     
     return 0;
