0e1cfb105b166766d23b22de66be4dfb100c69d1
Tom O'Mara <tom@winerepublic.com.au>
2023-08-02 23:14:36 +1000

    WIP: Processes Shell

--- Projects/processes-shell/wish.c
+++ Projects/processes-shell/wish.c
@@ -4,8 +4,10 @@
 #include <string.h>
 
 #define PROMPT_TEXT "wish> "
+#define LINE_TERMINATE '\0'
 #define EXIT_TEXT "exit"
 #define BUFFER_SIZE 1024
+#define DELIMITER ' '
 
 int main(int argc, char* argv[]) {
     
@@ -16,16 +18,19 @@ int main(int argc, char* argv[]) {
     size_t lineLen = 0;
 
     if (argc == 1)  {
-        printf("%s", PROMPT_TEXT);
         fileStream = stdin;
     }
     else {
         fileStream = stdin;
     }
 
-    while (strcmp(getline(&line, &lineLen, fileStream), EXIT_TEXT) != 0) {
-        //fgets(buffer, inputBuffer, stdin);
+    while (true) {
+        // Add ability to use cd
+        
         printf("%s", PROMPT_TEXT);
+        lineLen = getline(&line, &lineLen, fileStream); 
+        line[lineLen-1] = LINE_TERMINATE;
+        if (strcmp(line, EXIT_TEXT) == 0) exit(0);
     }
 
     return 0;
